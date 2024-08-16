3b7c1830dd0ea828280078ed91879ce19b7bfeb8
M-Ehsan-Ali <mrehsan51@gmail.com>
2024-08-16 18:56:42 +0500

    styled again

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,28 @@
 import React from "react";
 import profilePic from "../Assets/profilePic.jpeg";
+import "./home.css";
 
 const Home = () => (
   <section className="home">
-    <img src={profilePic} alt="Muhammad Ehsan Ali" className="profile-pic" />
-    <h1>Muhammad Ehsan Ali</h1>
-    <p>
-      I am a dedicated React JS Developer with over 4 years of hands-on
-      experience in building high-performance web applications. My passion for
-      innovation drives me to stay updated with the latest technologies and best
-      practices. I excel at transforming complex requirements into scalable
-      solutions and thrive in dynamic team environments.
-    </p>
+    <div className="home-content">
+      <div className="text-content">
+        <h1>Muhammad Ehsan Ali</h1>
+        <p>
+          I am a dedicated React JS Developer with over 4 years of hands-on
+          experience in building high-performance web applications. My passion
+          for innovation drives me to stay updated with the latest technologies
+          and best practices. I excel at transforming complex requirements into
+          scalable solutions and thrive in dynamic team environments.
+        </p>
+      </div>
+      <div className="profile-pic-container">
+        <img
+          src={profilePic}
+          alt="Muhammad Ehsan Ali"
+          className="profile-pic"
+        />
+      </div>
+    </div>
   </section>
 );
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,75 +5,74 @@ import {
   default as stitchImg,
 } from "../Assets/projects/peeredge.png"; // Ensure paths are correct
 import pickleTourImg from "../Assets/projects/pickletour.png"; // Ensure paths are correct
+import strettoImg from "../Assets/projects/stretto.png"; // Ensure paths are correct
+import "./projects.css";
+
+const projects = [
+  {
+    title: "PeerEdge",
+    description:
+      "PeerEdge is a cloud-based solution designed to enhance call connectivity with comprehensive control and real-time analytics. This platform simplifies the management of voice and data WAN functions by consolidating them into a single interface. It ensures scalability without the need for complex configurations and offers full visibility through the PeerEdge Portal. Key features include vendor and customer relationship management, detailed reporting, recording settings, latency reports, and advanced call control mechanisms.",
+    link: "https://hcc-dev.peeredge.com/",
+    image: pearEdgeImg,
+  },
+  {
+    title: "Stitch",
+    description:
+      "Stitch is a unified platform designed to manage global connectivity and empower IT leaders with robust digital infrastructure and business solutions. It is capable of handling over 50,000 voice calls per second, ensuring flawless connections for global enterprises. The platform provides comprehensive analytics and automation to support secure and efficient interconnections worldwide. Stitch acts as a bridge between vendors and customers, offering support for VOIP and voice services.",
+    link: "https://stitch-ebs-dev.46labs.com/",
+    image: stitchImg,
+  },
+  {
+    title: "Provisioning",
+    description:
+      "Provisioning is a versatile platform that bridges the gap between legacy communication systems and modern solutions. It provides administrators with complete control over voice and data services, real-time analytics, and traffic management. This platform enhances global communication services by offering seamless integration and management of both legacy and modern systems. It also provides administrative control over other platforms like Stitch and PeerEdge.",
+    link: "https://prov-dev.peeredge.com/",
+    image: provisioningImg,
+  },
+  {
+    title: "Stretto",
+    description:
+      "Stretto is a specialized digital solution tailored for bankruptcy management. It meets the complex needs of financial professionals by providing tools for overseeing and managing bankruptcy cases efficiently. The platform emphasizes precision, security, and accuracy in handling financial transactions and records. Key functionalities include debt tracking, asset allocation, and comprehensive reporting to ensure reliable bankruptcy management and resolution.",
+    link: "https://www.stretto.com/",
+    image: strettoImg,
+  },
+  {
+    title: "MegaMind Admin Panel",
+    description:
+      "The MegaMind Admin Panel is a web-based application designed for managing home tuition systems. It enables tutors and tutees to manage their accounts effectively. The platform includes features for job management, monthly billing (both prepaid and postpaid), class scheduling, and cancellation. Additionally, it offers communication tools such as chat and file-sharing between tutors and tutees, and a support chat for interactions between the admin lead and end-users.",
+    link: "https://megamind-admin.netlify.app",
+    image: pickleTourImg,
+  },
+  {
+    title: "Pickle Tour LLC",
+    description:
+      "Pickle Tour LLC is a tournament management system specifically for organizing and managing pickleball games. The system allows organizers to publish events and provide detailed information for player registration. It also features real-time score management by referees, ensuring accurate and timely tracking of game scores and results. The platform facilitates efficient event management and enhances the overall tournament experience for players and organizers.",
+    link: "https://www.pickletour.com/",
+    image: pickleTourImg,
+  },
+];
 
 const Projects = () => (
   <section className="projects">
-    <h2>Featured Projects</h2>
-    <div className="project">
-      <img src={pearEdgeImg} alt="PearEdge" className="project-img" />
-      <h3>PearEdge</h3>
-      <p>
-        Developed a cloud-based switch platform to enhance call connectivity and
-        traffic management. Features include real-time analytics, vendor and
-        customer management, and a unified control panel for voice and data WAN
-        functions.
-      </p>
-      <a
-        href="https://hcc-dev.peeredge.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        View Project
-      </a>
-    </div>
-    <div className="project">
-      <img src={stitchImg} alt="Stitch" className="project-img" />
-      <h3>Stitch</h3>
-      <p>
-        Created a unified platform for global connectivity management, handling
-        over 50,000 voice calls per second. The project features robust
-        analytics and automation for secure interconnections worldwide.
-      </p>
-      <a
-        href="https://stitch-ebs-dev.46labs.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        View Project
-      </a>
-    </div>
-    <div className="project">
-      <img src={provisioningImg} alt="Provisioning" className="project-img" />
-      <h3>Provisioning</h3>
-      <p>
-        Built a platform to bridge legacy communications with modern solutions,
-        providing admins with real-time analytics and control over global voice
-        and data services.
-      </p>
-      <a
-        href="https://prov-dev.peeredge.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        View Project
-      </a>
-    </div>
-    <div className="project">
-      <img src={pickleTourImg} alt="Pickle Tour LLC" className="project-img" />
-      <h3>Pickle Tour LLC</h3>
-      <p>
-        Developed a tournament management system for pickleball games, featuring
-        real-time score management, event publishing, and player registration
-        functionalities.
-      </p>
-      <a
-        href="https://www.stretto.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        View Project
-      </a>
-    </div>
+    {projects.map((project, index) => (
+      <div key={index} className="project">
+        <div className="project-content">
+          <h2>{project.title}</h2>
+          <p>{project.description}</p>
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            View Project
+          </a>
+        </div>
+        <div className="project-img-container">
+          <img
+            src={project.image}
+            alt={project.title}
+            className="project-img"
+          />
+        </div>
+      </div>
+    ))}
   </section>
 );
 
